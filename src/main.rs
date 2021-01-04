e99987a8e8ff86db83307b7fb7296d7159ae4d84
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2021-01-04 22:27:28 +0000

    TODO: Better renderpasses

--- src/main.rs
+++ src/main.rs
@@ -157,7 +157,27 @@ fn main() {
         .unwrap();
 
     log::info!("Window created");
-    let renderer = block_on(Renderer::new(&window, &backend));
+    let mut renderer;
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        renderer = block_on(Renderer::new(&window, &backend));
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init().expect("could not initialize logger");
+        use winit::platform::web::WindowExtWebSys;
+        // On wasm, append the canvas to the document body
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+        renderer = wasm_bindgen_futures::spawn_local(Renderer::new(&window, &backend));
+    }
     let renderer = Rc::new(RefCell::new(renderer));
     log::info!("Renderer created");
 
